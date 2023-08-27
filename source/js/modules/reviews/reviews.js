df88b17eacd4aa29325ce908cb24c3fcfefe643b
Anna <mynameisannanas@gmail.com>
2023-08-27 23:58:21 +0200

    fix: позиция в nav и слайдер в отзывах

--- source/js/modules/reviews/reviews.js
+++ source/js/modules/reviews/reviews.js
@@ -9,6 +9,22 @@ const createReviewsSlider = () => {
       nextEl: '.reviews__swiper-next',
       prevEl: '.reviews__swiper-prev',
     },
+    breakpoints: {
+      1200: {
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        initialSlide: 0,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+    },
   });
 };
 
