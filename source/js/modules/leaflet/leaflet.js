2e15d7c95580d5d5e4deca2f746c7ef9ac3666a1
Anna <mynameisannanas@gmail.com>
2023-09-06 19:46:15 +0200

    fix: карта

--- source/js/modules/leaflet/leaflet.js
+++ source/js/modules/leaflet/leaflet.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 let initLeaflet = () => {
-  const map = L.map('map', {scrollWheelZoom: false}).setView([55.774906445034176, 37.632641498705205], 14);
+  const map = L.map('map', {scrollWheelZoom: false, dragging: false}).setView([55.774906445034176, 37.632641498705205], 14);
   L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
